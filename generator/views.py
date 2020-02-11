7db836e089a6cce6f72f5af685abb37c6c71d49c
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-11 15:20:38 +0000

    Forms

--- generator/views.py
+++ generator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello there friend!')
+    return render(request, 'generator/home.html', {'password':'kdfgbkebfh34873iy57thugkenb'})
 
-def eggs(request):
-    return HttpResponse('<h1>Eggs are so tasty</h1>')
+def password(request):
+    return render(request, 'generator/password.html')
