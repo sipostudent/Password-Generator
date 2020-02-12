95b61906f51bea56540ce08cf94bc2f1ec8f0fe6
Sipo Charles <sipocharles@yahoo.co.uk>
2020-02-12 07:02:24 +0000

    Basic Bootstrap Styling

--- generator/views.py
+++ generator/views.py
@@ -5,10 +5,12 @@ import random
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html', {'password':'kdfgbkebfh34873iy57thugkenb'})
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
 
 def password(request):
-
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
     if request.GET.get('uppercase'):
